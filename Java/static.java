70386e7d94de98046cf4577807b9e16b50f157cb
ABOUD <gh.abd.com.207@gmail.com>
2024-02-20 17:34:59 +0300

    add simple code for static

---
+++ Java/static.java
@@ -0,0 +1,19 @@
+class Main{
+static class student{
+static int count = 0;
+student(){
+count +=1;
+}
+}
+
+
+
+public static void main(String args[]){
+student s1 = new student();
+student s2 = new student();
+student s3 = new student();
+student s4 = new student();
+student s5 = new student();
+System.out.print(s5.count);
+}
+}
